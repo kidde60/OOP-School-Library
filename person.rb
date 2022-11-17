52a94a9c4d17cf235a77257d684826cbd600fc02
kidde60 <kgeorgewilliam60@gmail.com>
2022-11-17 10:45:39 +0300

    Modify Person class

--- person.rb
+++ person.rb
@@ -1,14 +1,17 @@
-class Person
+require_relative 'nameable'
+class Person < Nameable
+  attr_accessor :name, :age
+  attr_reader :id
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
-
-  attr_accessor :name, :age
-  attr_reader :id
-
+  def correct_name
+    @name
+  end
   def can_use_services?
     @is_of_age || @parent_permission
   end
@@ -19,3 +22,32 @@ class Person
     @age >= 18
   end
 end
+
+# require 'securerandom'
+
+# class Person < Nameable
+#   attr_accessor :name, :age
+#   attr_reader :id
+
+#   def initialize(age, name = 'Unknown', parent_permission: true)
+#     super()
+#     @id = Random.rand(1..1000)
+#     @name = name
+#     @age = age
+#     @parent_permission = parent_permission
+#   end
+
+#   def correct_name
+#     @name
+#   end
+
+#   def can_use_services?
+#     @is_of_age || @parent_permission
+#   end
+
+#   private
+
+#   def of_age?
+#     @age >= 18
+#   end
+# end
