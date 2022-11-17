60bbff61b65c40605c4bcfcb8c0b7dbcbc9a7350
kidde60 <kgeorgewilliam60@gmail.com>
2022-11-18 02:46:00 +0300

    Add teacher

--- teacher.rb
+++ teacher.rb
@@ -2,7 +2,7 @@ require_relative 'person'
 
 class Teacher < Person
   def initialize(specialization)
-    super(age, name, parent_permission)
+    super(age, name)
     @specialization = specialization
   end
 
